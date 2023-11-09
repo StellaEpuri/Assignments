311e6d28c67281ea180ffc8863ea00328ad058be
StellaEpuri <137855187+StellaEpuri@users.noreply.github.com>
2023-11-09 18:41:48 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,25 @@
+
+ Test{
+
+public static void main(String args[]){//stella 12 25.5 true 
+
+String name=args[0];
+
+int i=Integer.parseInt(args[1]);
+int j=Integer.parseInt(args[2]);
+
+float f=Float.parseFloat(args[3]);
+
+boolean b=Boolean.parseBoolean(args[4]);
+
+int sum=i+j;
+
+System.out.println("sum is:"+sum);
+
+System.out.println(f);
+System.out.println(b);
+
+
+}
+
+}
